23352ca503f93de27636c08c686f1735e5e7bcba
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2025-07-01 06:26:45 +0700

    Change primary key type from Int to UUID for User, Product, Transaction, and TransactionItem models in Prisma schema and corresponding migration SQL.

---
+++ prisma/migrations/20250630232019_change_formad_id_from_int_to_uuid/migration.sql
@@ -0,0 +1,57 @@
+/*
+  Warnings:
+
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Transaction` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `TransactionItem` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TransactionItem" DROP CONSTRAINT "TransactionItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TransactionItem" DROP CONSTRAINT "TransactionItem_transactionId_fkey";
+
+-- AlterTable
+ALTER TABLE "Product" DROP CONSTRAINT "Product_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Product_id_seq";
+
+-- AlterTable
+ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Transaction_id_seq";
+
+-- AlterTable
+ALTER TABLE "TransactionItem" DROP CONSTRAINT "TransactionItem_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "transactionId" SET DATA TYPE TEXT,
+ALTER COLUMN "productId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "TransactionItem_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "TransactionItem_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TransactionItem" ADD CONSTRAINT "TransactionItem_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TransactionItem" ADD CONSTRAINT "TransactionItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
