2ba9dff785f0d31eb5eb320c15a5e3428d676b7c
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2025-07-18 16:12:13 +0700

    Add cascade delete constraints to Inventory, Transaction, and TransactionItem tables

---
+++ prisma/migrations/20250718073817_adding_cascade_delete/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "Inventory" DROP CONSTRAINT "Inventory_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TransactionItem" DROP CONSTRAINT "TransactionItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TransactionItem" DROP CONSTRAINT "TransactionItem_transactionId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TransactionItem" ADD CONSTRAINT "TransactionItem_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TransactionItem" ADD CONSTRAINT "TransactionItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Inventory" ADD CONSTRAINT "Inventory_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
