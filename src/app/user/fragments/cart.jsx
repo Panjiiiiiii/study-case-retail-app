501e1b68cdd2d92606c6063f148b7d5677959022
Panjiiiiiii <panjiputraardian@gmail.com>
2025-08-11 05:57:46 +0700

    Add CartList component: create initial structure for transaction details display

---
+++ src/app/user/fragments/cart.jsx
@@ -0,0 +1,16 @@
+import { H1 } from "@/components/ui/Text";
+
+export default function CartList (params) {
+    return (
+        <>
+            <div className="">
+                <aside className="flex flex-col justify-start h-full py-8">
+                    <H1>Detail Transaction</H1>
+                    <div>
+                        
+                    </div>
+                </aside>
+            </div>
+        </>
+    )
+};
