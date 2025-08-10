b682c8e43fa1bdb0ca10e0e7c1da164a430c81e4
Panjiiiiiii <panjiputraardian@gmail.com>
2025-08-11 05:57:39 +0700

    Refactor CardList component: replace dummy product data with API fetch and update MenuCard image source

--- src/app/user/components/card.jsx
+++ src/app/user/components/card.jsx
@@ -37,12 +37,12 @@ export default function MenuCard({ product }) {
 
     return (
         <>
-            <div className="flex-row p-2 bg-white w-[240px] h-[360px] rounded-xl shadow-md">
+            <div className="flex-row p-2 bg-white w-[280px] h-[360px] rounded-xl shadow-md">
                 <div className="flex flex-col items-center justify-center h-full">
                     <img 
-                        src={product?.image || "/kecap.png"} 
+                        src={product?.imageUrl || "/kecap.png"} 
                         alt="Menu Image" 
-                        className="w- h-auto object-cover rounded-t-xl mb-4"
+                        className="w-[120px] h-[120px] object-cover rounded-t-xl mb-4"
                     />
                     <H2 className="text-[18px] font-bold text-sky-950 mb-1">
                         {product?.name || "Kecap Bango 180 ML"}
