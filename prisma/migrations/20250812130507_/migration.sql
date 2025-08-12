c5445b7b1bc615607f8224f7997dac31012038b0
Panjiiiiiii <panjiputraardian@gmail.com>
2025-08-12 20:06:09 +0700

    Remove status column and related enums from Transaction model

---
+++ prisma/migrations/20250812130507_/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Transaction` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Transaction" DROP COLUMN "status";
+
+-- DropEnum
+DROP TYPE "PaymentStatus";
+
+-- DropEnum
+DROP TYPE "TransactionStatus";
